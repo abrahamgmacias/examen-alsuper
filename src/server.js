bba89cb1fe388c0ce0ead51461ed0294ba3c6087
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-11-11 17:15:40 -0600

    setups mysql database

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const { app } = require('./app');
+const db = require("./database/models/index");
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+    db.sequelize.sync();
     console.log(`Listening on ${PORT}...`);
 });
