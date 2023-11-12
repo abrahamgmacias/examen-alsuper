724bfb19a158382032d78a72ad4220e9ea9b8338
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-11-12 13:32:03 -0600

    enables autor-libro relationship

--- src/database/migrations/20231112034645-create-libros.js
+++ src/database/migrations/20231112034645-create-libros.js
@@ -17,7 +17,7 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: true
       },
-      autor: {
+      autor_id: {
         type: Sequelize.STRING,
         allowNull: false
       },
