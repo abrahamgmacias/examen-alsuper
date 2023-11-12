229a0f57a484de5d20a0f2d4c7bcf6a6fc0cc847
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-11-12 16:23:07 -0600

    renamed to usuarios to auth

---
+++ src/controllers/auth.js
@@ -0,0 +1,29 @@
+const { usuarios } = require("../database/models/index");
+const { crearSessionJWT, validarSessionJWT } = require("../util/jwt");
+
+// La contraseñas no están encriptadas
+async function login(correo_electronico, contrasena) {
+    userData = await usuarios.findOne({
+        where: { correo_electronico }
+    });
+
+    // Validar si no existe el usuario o si la contraseña es incorrecta
+    if (userData === null || !(userData.dataValues.contrasena === contrasena)) {
+        return { success: false, token: null };
+    }
+
+    // Crear token de sesión
+    const token = crearSessionJWT({
+        id: userData.dataValues.id,
+        name: userData.dataValues.name
+    });
+
+    const { id, nombre, apellido_paterno } = userData;
+
+    return { success: true, id, token, nombre, apellido_paterno };
+}
+
+
+
+
+module.exports = { login };
