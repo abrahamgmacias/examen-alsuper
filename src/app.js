9d8dfa773ef9658a282e0ca0d4274ab2d0d9c1a2
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-11-12 16:23:13 -0600

    renamed usuarios to auth

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ const cors = require('cors');
 require('dotenv').config();
 
 const navegacionRouter = require('./routes/navegacion');
-const usuariosRouter = require('./routes/usuarios');
 const autoresRouter = require('./routes/autores');
 const librosRouter = require('./routes/libros');
+const authRouter = require('./routes/auth');
 
 // App instantiation
 const app = express();
@@ -15,8 +15,8 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
+app.use("/auth", authRouter);
 app.use("/", navegacionRouter);
-app.use("/auth", usuariosRouter);
 app.use("/libros", librosRouter);
 app.use("/autores", autoresRouter);
 
