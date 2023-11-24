a659508098e869e76164869fd98e2874207fa905
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-11-24 17:37:12 -0600

    adds api

--- src/app.js
+++ src/app.js
@@ -17,10 +17,10 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-app.use("/auth", authRouter);
-app.use("/", navegacionRouter);
-app.use("/libros", librosRouter);
-app.use("/autores", autoresRouter);
-app.use("/usuarios", usuariosRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/", navegacionRouter);
+app.use("/api/libros", librosRouter);
+app.use("/api/autores", autoresRouter);
+app.use("/api/usuarios", usuariosRouter);
 
 module.exports = { app };
